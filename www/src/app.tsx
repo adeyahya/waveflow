fa2ec6ff9dc8d04e491df9520834b199cf9a0284
adeyahya <adeyahyaprasetyo@gmail.com>
2021-06-10 04:40:44 +0700

    user fetch

--- www/src/app.tsx
+++ www/src/app.tsx
@@ -1,33 +1,45 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useMount } from "react-use";
 
 import Root from "~pages";
 import Login from "~pages/login";
 import NotFound from "~pages/not-found";
 import NewWorkflow from "~pages/workflows/new";
+import useUserStore from "~store/user";
 
-const App = (
-  <ChakraProvider>
-    <React.StrictMode>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Root />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/workflow/new">
-            <NewWorkflow />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router>
-    </React.StrictMode>
-  </ChakraProvider>
-);
+const App = () => {
+  const user = useUserStore();
+
+  useMount(() => {
+    user.fetch();
+  });
+
+  if (user.loading) return null;
+
+  return (
+    <ChakraProvider>
+      <React.StrictMode>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Root />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/workflow/new">
+              <NewWorkflow />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Router>
+      </React.StrictMode>
+    </ChakraProvider>
+  );
+};
 
 export default App;
--- www/src/store/user.ts
+++ www/src/store/user.ts
@@ -1,12 +1,29 @@
 import create from 'zustand'
+import axios from 'axios';
 
 type User = {
-  username: string | null
+  username: string | null;
+  email: string | null;
+  loading: boolean;
+  set: (user: User) => void;
+  fetch: () => Promise<void>;
 }
 
 const useUserStore = create<User>(set => ({
   username: null,
+  email: null,
+  loading: false,
   set: (user: User) => set(user),
+  fetch: async () => {
+    try {
+      set({ loading: true });
+      const { data } = await axios.get<User>('http://localhost:3001/users/me');
+      set({ ...data, loading: false })
+    } catch { }
+    finally {
+      set({ loading: false })
+    }
+  }
 }))
 
 export default useUserStore;
